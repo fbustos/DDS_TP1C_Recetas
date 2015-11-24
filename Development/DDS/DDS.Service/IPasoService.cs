0c2d66a13956ae4a58af7279baa04e9c34e905b0
fbustos <francobustos07@gmail.com>
2015-11-24 17:44:30 -0300

    Faltaban incluir algunos cambios

---
+++ Development/DDS/DDS.Service/IPasoService.cs
@@ -0,0 +1,16 @@
+﻿using DDS.Model.Models;
+using System.Collections.Generic;
+
+namespace DDS.Service
+{
+    // operations you want to expose
+    public interface IPasoService
+    {
+        IEnumerable<Paso> GetPasos();
+        Paso Get(int id);
+        void Create(Paso paso);
+        void Save();
+        void Update(Paso paso);
+        void Delete(Paso paso);
+    }
+}
---
+++ Development/DDS/DDS.Web/Models/ViewModels/PasoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+
+namespace DDS.Models.ViewModels
+{
+    public class PasoViewModel
+    {
+        public int Id { get; set; }
+
+        public int RecetaId { get; set; }
+
+        public int Numero { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public HttpPostedFileBase Imagen { get; set; }
+
+        public string ImagenPath { get; set; }
+
+        public string Code
+        {
+            get { return "REC_" + this.RecetaId; }
+        }
+
+        public string NombreImagen
+        {
+            get { return "Paso_" + this.Numero; }
+        }
+    }
+}
